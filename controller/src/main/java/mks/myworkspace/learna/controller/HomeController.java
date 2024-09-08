42e87099ad9386b8ce958344992f7546aa5bd026
Huỳnh Nam Duy <duyvnlx3016@gmail.com>
2024-09-08 18:49:29 +0700

    Add courseItem fragment Split script fragment

--- controller/src/main/java/mks/myworkspace/learna/controller/HomeController.java
+++ controller/src/main/java/mks/myworkspace/learna/controller/HomeController.java
@@ -1,51 +1,26 @@
-/**
- * Licensed to MKS Group under one or more contributor license
- * agreements. See the NOTICE file distributed with this work
- * for additional information regarding copyright ownership.
- * MKS Group licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a
- * copy of the License at:
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package mks.myworkspace.learna.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import mks.myworkspace.learna.service.JobRoleService;
+import mks.myworkspace.learna.entity.Course;
+import mks.myworkspace.learna.service.CourseService;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
-public class HomeController extends BaseController {
-	
-	@Autowired
-	JobRoleService jobRoleService;
- 
-	   /**
-     * This method is called when binding the HTTP parameter to bean (or model).
-     * 
-     * @param binder
-     */
+@RequestMapping(value = {"/", "/home"})
+public class HomeController {
+
+    @Autowired
+    private CourseService courseService;
+    
     @InitBinder
     protected void initBinder(WebDataBinder binder) {
         // Sample init of Custom Editor
@@ -55,20 +30,22 @@ public class HomeController extends BaseController {
 //        binder.registerCustomEditor((Class<List<ItemKine>>)(Class<?>)List.class, orderNoteEditor);
 
     }
-    
-	/**
-	 * Simply selects the home view to render by returning its name.
-     * @return 
-	 */
-	@RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
-	public ModelAndView displayHome(HttpServletRequest request, HttpSession httpSession) {
+
+    @GetMapping
+    public ModelAndView getAllCourses( ) {
 		ModelAndView mav = new ModelAndView("home");
 
-		initSession(request, httpSession);
-		
-		mav.addObject("currentSiteId", getCurrentSiteId());
-		mav.addObject("userDisplayName", getCurrentUserDisplayName());
+        List<Course> courses = courseService.getAllCourses();
+        mav.addObject("courses", courses);
+        System.out.println("Danh sách khóa học: " + courses);
 
-		return mav;
-	}
+        return mav;
+    }
+
+
+    @GetMapping("/courses")
+    @ResponseBody
+    public List<Course> getCourses() {
+        return courseService.getAllCourses();
+    }
 }
