c310828b3e1ad13dd03f5a436e4ef9cbdb1c7cdb
Huỳnh Nam Duy <duyvnlx3016@gmail.com>
2024-09-09 12:57:55 +0700

    Add campaign

--- controller/src/main/java/mks/myworkspace/learna/controller/HomeController.java
+++ controller/src/main/java/mks/myworkspace/learna/controller/HomeController.java
@@ -3,12 +3,10 @@ package mks.myworkspace.learna.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import mks.myworkspace.learna.entity.Course;
@@ -40,12 +38,5 @@ public class HomeController {
         System.out.println("Danh sách khóa học: " + courses);
 
         return mav;
-    }
-
-
-    @GetMapping("/courses")
-    @ResponseBody
-    public List<Course> getCourses() {
-        return courseService.getAllCourses();
     }
 }
