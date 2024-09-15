fe7c707a788534fbb5b3f420d0d448b5c0f9084f
trongdung721 <91901239+trongdung721@users.noreply.github.com>
2024-09-15 18:31:41 +0700

    Add Pagination Comment

--- controller/src/main/java/mks/myworkspace/learna/controller/PlayController.java
+++ controller/src/main/java/mks/myworkspace/learna/controller/PlayController.java
@@ -1,71 +1,46 @@
-/**
- * Licensed to MKS Group under one or more contributor license
- * agreements. See the NOTICE file distributed with this work
- * for additional information regarding copyright ownership.
- * MKS Group licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a
- * copy of the License at:
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package mks.myworkspace.learna.controller;
 
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-/**
- * Handles requests for the application home page.
- */
+import mks.myworkspace.learna.entity.Lesson;
+import mks.myworkspace.learna.service.PlayService;
+
 @Controller
-public class PlayController extends BaseController {
- 
-	   /**
-     * This method is called when binding the HTTP parameter to bean (or model).
-     * 
-     * @param binder
-     */
-    @InitBinder
-    protected void initBinder(WebDataBinder binder) {
-        // Sample init of Custom Editor
+public class PlayController {
 
-//        Class<List<ItemKine>> collectionType = (Class<List<ItemKine>>)(Class<?>)List.class;
-//        PropertyEditor orderNoteEditor = new MotionRuleEditor(collectionType);
-//        binder.registerCustomEditor((Class<List<ItemKine>>)(Class<?>)List.class, orderNoteEditor);
+    @Autowired
+    private PlayService playService;
 
+    @GetMapping("/play/{courseId}")
+    public ModelAndView displayCourseLessons(@PathVariable Long courseId, HttpServletRequest request, HttpSession httpSession) {
+        ModelAndView mav = new ModelAndView("play");
+        
+        List<Lesson> lessons = playService.getLessonsByCourseId(courseId);
+        mav.addObject("lessons", lessons); // Luôn luôn thêm lessons, kể cả khi nó rỗng
+        
+        // Kiểm tra và thêm thuộc tính currentSiteId nếu tồn tại
+        Object currentSiteId = request.getSession().getAttribute("currentSiteId");
+        if (currentSiteId != null) {
+            mav.addObject("currentSiteId", currentSiteId);
+        } else {
+            mav.addObject("currentSiteId", "Not Set"); // Hoặc bạn có thể set một giá trị mặc định
+        }
+        
+        // Kiểm tra và thêm userDisplayName nếu người dùng đã đăng nhập
+        if (request.getUserPrincipal() != null) {
+            mav.addObject("userDisplayName", request.getUserPrincipal().getName());
+        } else {
+            mav.addObject("userDisplayName", "Guest"); // Hoặc bạn có thể set một giá trị mặc định
+        }
+
+        return mav;
     }
-    
-	/**
-	 * Simply selects the home view to render by returning its name.
-     * @return 
-	 */
-	@GetMapping(value = "/play")
-	public ModelAndView displayHome(HttpServletRequest request, HttpSession httpSession) {
-		ModelAndView mav = new ModelAndView("play");
-
-		initSession(request, httpSession);
-		
-		mav.addObject("currentSiteId", getCurrentSiteId());
-		mav.addObject("userDisplayName", getCurrentUserDisplayName());
-
-		return mav;
-	}
 }
