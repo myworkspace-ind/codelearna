590c9bfa05d2da72fed0af93f15d83007b094dc7
trongdung721 <91901239+trongdung721@users.noreply.github.com>
2024-09-28 21:56:14 +0700

    Add save course controller

---
+++ controller/src/main/java/mks/myworkspace/learna/controller/AdminCourseController.java
@@ -0,0 +1,34 @@
+package mks.myworkspace.learna.controller;
+
+import mks.myworkspace.learna.entity.Course;
+import mks.myworkspace.learna.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminCourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    // Hiển thị trang thêm khóa học
+    @GetMapping("/save-course")
+    public ModelAndView showSaveCoursePage() {
+    	ModelAndView mav = new ModelAndView("Admin/addCourse");
+        mav.addObject("course", new Course()); 
+        return mav;
+    }
+
+    // Xử lý lưu khóa học
+    @PostMapping("/save-course")
+    public String saveCourse(@ModelAttribute("course") Course course) {
+        courseService.saveCourse(course);
+        return "redirect:/admin/save-course"; // Sau khi lưu, quay lại trang form
+    }
+}
