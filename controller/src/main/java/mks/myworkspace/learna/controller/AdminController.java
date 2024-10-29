3e080b549db32da79fa1e6a4aebdd4835746f1a5
Võ Hữu Tài <90029952+vohuutai23@users.noreply.github.com>
2024-10-29 12:30:43 +0700

    Update AdminController.java

--- controller/src/main/java/mks/myworkspace/learna/controller/AdminController.java
+++ controller/src/main/java/mks/myworkspace/learna/controller/AdminController.java
@@ -497,7 +497,7 @@ public class AdminController {
 
 	@GetMapping("/addParameterHandsontable")
 	public ModelAndView showAddParameterHandsontablePage() {
-		ModelAndView mav = new ModelAndView("fragments/adminAddParametersHandsontable");
+		ModelAndView mav = new ModelAndView("fragments/adminAddParameterHandsontable");
 		return mav;
 	}
 
