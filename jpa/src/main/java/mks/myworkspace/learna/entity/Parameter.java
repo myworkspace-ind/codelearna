371e921d78df3158b3edb8c19a43b2022a1feefc
ledinhloc <ledinhloc7@gmail.com>
2024-11-16 13:41:57 +0700

    hoan thanh issue1 : them truong bat buoc, issue2: them truong seqno de sap xep parameter

--- jpa/src/main/java/mks/myworkspace/learna/entity/Parameter.java
+++ jpa/src/main/java/mks/myworkspace/learna/entity/Parameter.java
@@ -7,23 +7,26 @@ import lombok.Data;
 @Entity
 @Data
 @Table(name = "learna_parameter", uniqueConstraints = {
-	    @UniqueConstraint(columnNames = {"param_key", "param_value"}) 
-	})
+		@UniqueConstraint(columnNames = { "param_key", "param_value" }) })
 
 public class Parameter {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(name = "param_key", nullable = false)
 	private String paramKey;
-	
+
 	@Column(name = "param_value", nullable = false)
 	private String paramValue;
+
 	
+	@Column(name = "seqno")
+	private Integer seqno;
+	
+
 	@Column(name = "description")
 	private String description;
-	
-	
+
 }
