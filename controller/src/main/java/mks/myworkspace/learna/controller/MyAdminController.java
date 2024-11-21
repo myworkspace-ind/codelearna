e665394c14a1b3eab491557741022331c05d0ec8
ledinhloc <ledinhloc7@gmail.com>
2024-11-21 23:01:10 +0700

    update

--- controller/src/main/java/mks/myworkspace/learna/controller/MyAdminController.java
+++ controller/src/main/java/mks/myworkspace/learna/controller/MyAdminController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import mks.myworkspace.learna.service.CourseService;
 import mks.myworkspace.learna.service.ParameterService;
 
 @Controller
@@ -19,6 +20,8 @@ public class MyAdminController extends BaseController {
 	@Autowired
 	private ParameterService parameterService;
 	
+	@Autowired
+	private CourseService courseService;
 	@GetMapping("")
 	public String showSettings(HttpServletRequest request, HttpSession httpSession) {
 		super.initSession(request, httpSession);
@@ -38,6 +41,14 @@ public class MyAdminController extends BaseController {
 	public String loadParametersList(HttpServletRequest request, HttpSession httpSession, Model model) {
 	    httpSession.setAttribute("currentMenu", "Parameters");
 	    model.addAttribute("parameters", parameterService.getAllParams()); 
+	    return "myadmin";
+	}
+
+	@GetMapping("/course-management")
+	public String loadCourseList(HttpServletRequest request, HttpSession httpSession, Model model) {
+	    httpSession.setAttribute("currentMenu", "Course"); 
+	    model.addAttribute("courses", courseService.getAllCourses()); 
+
 	    return "myadmin";
 	}
 }
