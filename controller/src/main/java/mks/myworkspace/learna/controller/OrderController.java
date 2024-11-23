305893cfb0104888206b806c66f393910a49578b
minhkhoavo <v.minhkhoa123456@gmail.com>
2024-11-23 16:02:21 +0700

    build: qr code payment page, add check payment status in payment controller, fix render qr code page

--- controller/src/main/java/mks/myworkspace/learna/controller/OrderController.java
+++ controller/src/main/java/mks/myworkspace/learna/controller/OrderController.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/orders")
-public class OrderController {
+public class OrderController extends BaseController {
     @Autowired
     private OrderService orderService;
 
@@ -22,19 +25,23 @@ public class OrderController {
 
     @PostMapping
     public String processOrder(
+    		HttpServletRequest request, 
+    		HttpSession httpSession,
             @RequestParam String paymentMethod,
             @RequestParam BigDecimal amount,
             @RequestParam Long userId,
             @RequestParam Long courseId,
             Model model) {
+    	// System.out.println("userEid:::::" + getCurrentUserEid());
 
+    	// userId = getCurrentUserEid();
         Order order = orderService.createOrder(paymentMethod, amount, userId, courseId);
         // System.out.println(order);
         if ("transfer".equals(paymentMethod)) {
             String qrCodeUrl = orderService.generateQrCodeUrl(order.getOrderCode(), amount);
             model.addAttribute("orderId", order.getOrderCode());
             model.addAttribute("qrCodeUrl", qrCodeUrl);
-            return "qr-code-view";
+            return "fragments/qr-code-payment";
         } else if ("vnpay".equals(paymentMethod)) {
             return "redirect:/orders/";
         }
--- controller/src/main/java/mks/myworkspace/learna/controller/PaymentController.java
+++ controller/src/main/java/mks/myworkspace/learna/controller/PaymentController.java
@@ -1,16 +1,31 @@
 package mks.myworkspace.learna.controller;
 
+import mks.myworkspace.learna.entity.Order;
+import mks.myworkspace.learna.service.OrderService;
 import mks.myworkspace.learna.service.PaymentService;
 import mks.myworkspace.learna.service.UserLibraryCourseService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -25,6 +40,14 @@ public class PaymentController extends BaseController {
 
     @Autowired
     private UserLibraryCourseService userLibraryCourseService;
+    @Autowired
+    private OrderService orderService;
+    
+    private final RestTemplate restTemplate = new RestTemplate();
+
+
+    @Value("${payment.sepay.apiKey}")
+    private String bearerToken;
 
     @GetMapping("/balance")
     public ModelAndView getBalance(HttpServletRequest request, HttpSession httpSession) {
@@ -57,5 +80,42 @@ public class PaymentController extends BaseController {
         }
         return mav;
     }
+    
+    @GetMapping("/check/{orderCode}")
+    public ResponseEntity<String> processPayment(@PathVariable String orderCode) {
+        String url = "https://my.sepay.vn/userapi/transactions/list?limit=20";
 
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + bearerToken);
+            HttpEntity<String> entity = new HttpEntity<>(headers);
+
+            ResponseEntity<Map> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, Map.class);
+            Map<String, Object> response = responseEntity.getBody();
+
+            List<Map<String, Object>> transactions = (List<Map<String, Object>>) response.get("transactions");
+
+            if (transactions != null && !transactions.isEmpty()) {
+                for (Map<String, Object> transaction : transactions) {
+                    String code = (String) transaction.get("code");
+
+                    if (code != null && code.equals(orderCode)) {
+                        Optional<Order> order = orderService.getOrder(code);
+                        BigDecimal transactionAmount = new BigDecimal((String) transaction.get("amount_in"));
+                        if (order.isPresent() && order.get().getAmount().compareTo(transactionAmount) == 0) {
+                            orderService.updateOrderStatus(order.get().getOrderCode(), Order.OrderStatus.COMPLETED);
+                            return ResponseEntity.ok("PAID");
+                        }
+                    }
+                }
+            }
+
+            return ResponseEntity.ok(orderCode + " not found");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An error occurred: " + e.getMessage());
+        }
+    }
 }
