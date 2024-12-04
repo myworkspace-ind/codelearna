887b0a051de75eb9ae5bb61821f1bd8182eeba8c
trongdung721 <91901239+trongdung721@users.noreply.github.com>
2024-12-05 02:54:35 +0700

    add status data (delete -> disable)

--- web/src/main/webapp/resources/js/adminHome.js
+++ web/src/main/webapp/resources/js/adminHome.js
@@ -1,8 +1,11 @@
 
 import { initializePagination } from './pagination.js';
 import { showSuccessToast, showErrorToast } from './toast.js';
-
+import {showSpinnerLoading, showBarLoading, hideAllLoading} from './loading.js'
 
 window.initializePagination = initializePagination;
 window.showSuccessToast = showSuccessToast;
 window.showErrorToast = showErrorToast;
+window.showSpinnerLoading = showSpinnerLoading;
+window.showBarLoading = showBarLoading;
+window.hideAllLoading = hideAllLoading;
