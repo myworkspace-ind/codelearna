e0fd2b2e2947b2583be57ee56d8455ab995b0451
Võ Hữu Tài <90029952+vohuutai23@users.noreply.github.com>
2024-12-04 16:26:43 +0700

    modify admin sidebar UI

--- web/src/main/webapp/resources/js/adminHome.js
+++ web/src/main/webapp/resources/js/adminHome.js
@@ -6,3 +6,21 @@ import { showSuccessToast, showErrorToast } from './toast.js';
 window.initializePagination = initializePagination;
 window.showSuccessToast = showSuccessToast;
 window.showErrorToast = showErrorToast;
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleSidebarBtn = document.querySelector(".toggle-sidebar");
+    const sidebar = document.querySelector(".sidebar");
+
+    toggleSidebarBtn.addEventListener("click", function () {
+        sidebar.classList.toggle("collapsed");
+    });
+});
+
+
+document.querySelectorAll('.sidebar nav a').forEach(link => {
+    link.addEventListener('click', function () {
+        document.querySelectorAll('.sidebar nav a').forEach(el => el.classList.remove('active'));
+        this.classList.add('active');
+    });
+})
