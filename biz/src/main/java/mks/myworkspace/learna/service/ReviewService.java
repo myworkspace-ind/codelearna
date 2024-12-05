397bbbd4369c23a1cbd6bcee90b8c2c911b4d60b
Võ Hữu Tài <90029952+vohuutai23@users.noreply.github.com>
2024-12-05 17:14:24 +0700

    add review filters

--- biz/src/main/java/mks/myworkspace/learna/service/ReviewService.java
+++ biz/src/main/java/mks/myworkspace/learna/service/ReviewService.java
@@ -3,6 +3,7 @@ package mks.myworkspace.learna.service;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import mks.myworkspace.learna.entity.Review;
 import mks.myworkspace.learna.repository.ReviewRepository;
@@ -10,7 +11,7 @@ import mks.myworkspace.learna.repository.ReviewRepository;
 public interface ReviewService {
 	
 //	List<Review> getReviewsByCourseId(Long courseId);
-	Page<Review> getReviewsByCourseId(Long courseId, int page, int size);
+	List<Review> getFilteredReviews(Long courseId, String sortBy);
 	void addReview(Review review, Long courseId);
 	void deleteReviewById(Long reviewId, Long courseId);
 	void updateReviewById(Long reviewId, Review review);
