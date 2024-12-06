0c5040a0a86c749eb59143b847d28917065edd12
Võ Hữu Tài <90029952+vohuutai23@users.noreply.github.com>
2024-12-07 01:16:55 +0700

    add welcome admin page

--- web/src/main/webapp/resources/js/adminHome.js
+++ web/src/main/webapp/resources/js/adminHome.js
@@ -1,29 +1,55 @@
 
 import { initializePagination } from './pagination.js';
 import { showSuccessToast, showErrorToast } from './toast.js';
-import {showSpinnerLoading, showBarLoading, hideAllLoading} from './loading.js'
+import { showSpinnerLoading, showBarLoading, hideAllLoading } from './loading.js'
 
 window.initializePagination = initializePagination;
 window.showSuccessToast = showSuccessToast;
 window.showErrorToast = showErrorToast;
 
 
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleSidebarBtn = document.querySelector(".toggle-sidebar");
-    const sidebar = document.querySelector(".sidebar");
+document.addEventListener("DOMContentLoaded", function() {
+	const toggleSidebarBtn = document.querySelector(".toggle-sidebar");
+	const sidebar = document.querySelector(".sidebar");
 
-    toggleSidebarBtn.addEventListener("click", function () {
-        sidebar.classList.toggle("collapsed");
-    });
+	toggleSidebarBtn.addEventListener("click", function() {
+		sidebar.classList.toggle("collapsed");
+	});
 });
 
 
 document.querySelectorAll('.sidebar nav a').forEach(link => {
-    link.addEventListener('click', function () {
-        document.querySelectorAll('.sidebar nav a').forEach(el => el.classList.remove('active'));
-        this.classList.add('active');
-    });
+	link.addEventListener('click', function() {
+		document.querySelectorAll('.sidebar nav a').forEach(el => el.classList.remove('active'));
+		this.classList.add('active');
+	});
 })
+
+// check dynamic-content to display the welcome page
+document.addEventListener('DOMContentLoaded', function() {
+	const dynamicContent = document.getElementById('dynamic-content');
+	const welcomeSection = document.getElementById('welcomeSection');
+
+	welcomeSection.style.display = 'block';
+
+	function updateVisibility() {
+		if (dynamicContent.innerHTML.trim() !== '') {
+			welcomeSection.style.display = 'none';
+		} else {
+			welcomeSection.style.display = 'block';
+		}
+	}
+
+	const observer = new MutationObserver(updateVisibility);
+
+	observer.observe(dynamicContent, {
+		childList: true,
+		characterData: true,
+		subtree: true
+	});
+
+	updateVisibility();
+});
 
 window.showSpinnerLoading = showSpinnerLoading;
 window.showBarLoading = showBarLoading;
